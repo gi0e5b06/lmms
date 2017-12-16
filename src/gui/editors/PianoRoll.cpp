7d31aa261d637670dda0b5e4f5fb277628fa4089
Oskar Wallgren <oskar.wallgren13@gmail.com>
2017-12-17 00:38:37 +0100

    Piano Roll - Don't quantize when pasting notes
    Some changes on pasting notes in the Piano Roll:
    * Don't quantize notes when pasting
    * Add journal checkpoint
    * Set project changed

--- src/gui/editors/PianoRoll.cpp
+++ src/gui/editors/PianoRoll.cpp
@@ -3768,7 +3768,7 @@ void PianoRoll::pasteNotes()
 			cur_note.setSelected( true );
 
 			// add to pattern
-			m_pattern->addNote( cur_note );
+			m_pattern->addNote( cur_note, false );
 		}
 
 		// we only have to do the following lines if we pasted at
@@ -3931,6 +3931,8 @@ void PianoRoll::quantizeNotes()
 		return;
 	}
 
+	m_pattern->addJournalCheckPoint();
+
 	NoteVector notes = getSelectedNotes();
 
 	if( notes.empty() )
@@ -3956,6 +3958,7 @@ void PianoRoll::quantizeNotes()
 
 	update();
 	gui->songEditor()->update();
+	Engine::getSong()->setModified();
 }
 
 
