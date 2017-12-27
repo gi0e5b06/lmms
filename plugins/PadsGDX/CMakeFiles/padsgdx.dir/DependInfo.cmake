0944c62db5e83aa87f51819fd9b6337272edb06b
gi0e5b06 <gi0e5b06@users.noreply.github.com>
2017-12-27 09:10:45 +0100

    .

---
+++ plugins/PadsGDX/CMakeFiles/padsgdx.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/desnoix/Devel/LMMS/lmms/plugins/PadsGDX/PadsGDX.cpp" "/home/desnoix/Devel/LMMS/lmms/plugins/PadsGDX/CMakeFiles/padsgdx.dir/PadsGDX.cpp.o"
+  "/home/desnoix/Devel/LMMS/lmms/plugins/PadsGDX/PadsGDXView.cpp" "/home/desnoix/Devel/LMMS/lmms/plugins/PadsGDX/CMakeFiles/padsgdx.dir/PadsGDXView.cpp.o"
+  "/home/desnoix/Devel/LMMS/lmms/plugins/PadsGDX/PadsGDXWaveView.cpp" "/home/desnoix/Devel/LMMS/lmms/plugins/PadsGDX/CMakeFiles/padsgdx.dir/PadsGDXWaveView.cpp.o"
+  "/home/desnoix/Devel/LMMS/lmms/plugins/PadsGDX/moc_PadsGDX.cpp" "/home/desnoix/Devel/LMMS/lmms/plugins/PadsGDX/CMakeFiles/padsgdx.dir/moc_PadsGDX.cpp.o"
+  "/home/desnoix/Devel/LMMS/lmms/plugins/PadsGDX/moc_PadsGDXView.cpp" "/home/desnoix/Devel/LMMS/lmms/plugins/PadsGDX/CMakeFiles/padsgdx.dir/moc_PadsGDXView.cpp.o"
+  "/home/desnoix/Devel/LMMS/lmms/plugins/PadsGDX/moc_PadsGDXWaveView.cpp" "/home/desnoix/Devel/LMMS/lmms/plugins/PadsGDX/CMakeFiles/padsgdx.dir/moc_PadsGDXWaveView.cpp.o"
+  "/home/desnoix/Devel/LMMS/lmms/plugins/PadsGDX/qrc_padsgdx.cpp" "/home/desnoix/Devel/LMMS/lmms/plugins/PadsGDX/CMakeFiles/padsgdx.dir/qrc_padsgdx.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "PLUGIN_NAME=padsgdx"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_WIDGETS_LIB"
+  "QT_XML_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++-64"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtXml"
+  "plugins/PadsGDX"
+  "."
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
